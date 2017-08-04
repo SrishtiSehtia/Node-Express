1d13e74f9e6cb5fb7a5c088939d3bd9b41ae51b7
Srishti Sehtia <srishti0123@gmail.com>
2017-08-04 05:23:13 +0000

    create movie results route

--- movie_search_app/app.js
+++ movie_search_app/app.js
@@ -3,10 +3,19 @@ var app = express();
 var request = require("request");
 var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true})); 
+
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res) {
+   res.render("search"); 
+});
+
 app.get("/results", function(req, res){
-    request("http://www.omdbapi.com/?s=star&apikey=thewdb",function(error, response, body){
+    var query = req.query.movie;
+    var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb";
+    console.log(url);
+    request(url, function(error, response, body){
         if(!error && response.statusCode == 200) {
             var data = JSON.parse(body)
             res.render("results", {data: data});
